966b95d4246a4512b42c8186b183e64a36cadcd1
Iury Vasconcelos <iury0393@gmail.com>
2022-08-11 18:53:28 -0300

    uploading pod/alamofire

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '13.0'
+
+target 'caats_app' do
+  use_frameworks!
+
+  # Pods for caats_app
+  pod 'Alamofire', '~> 5.6'
+
+end
---
+++ Pods/Target Support Files/Pods-caats_app/Pods-caats_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_caats_app : NSObject
+@end
+@implementation PodsDummy_Pods_caats_app
+@end
