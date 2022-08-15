1c9df7c4a660f5755162b39027b0d56df10c31cf
Iury Vasconcelos <iury0393@gmail.com>
2022-08-15 17:13:11 -0300

    Adicionando AlamofireImage e AlamofireNetworkActivityIndicator

--- caats_app/AppDelegate.swift
+++ caats_app/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseCore
 import IQKeyboardManagerSwift
+import AlamofireNetworkActivityIndicator
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        NetworkActivityIndicatorManager.shared.isEnabled = true
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true
